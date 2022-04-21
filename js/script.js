00b28e0ea608a24f7b2f259aa1fb2c440fecb7a0
SVA2021 <sofronov.vit@gmail.com>
2022-04-21 22:58:18 +0400

    gallery of project card style and script finish

--- js/script.js
+++ js/script.js
@@ -12,24 +12,33 @@ headerMenuBtn.addEventListener('click', (e) => {
 
 //*******************PROJECT **********************/
 
-const gallery = document.body.querySelector('.project__gallery');
-const imageArray = gallery.querySelectorAll('.project__pic');
+const galleryArray = document.body.querySelectorAll('.project__gallery');
+// const gallery = document.body.querySelector('.project__gallery');
 
-const intervalLog = async (array) => {
+// gallery.addEventListener('click', () => gallery.classList.toggle('showFull'));
+
+const slideShow = async (array) => {
 
 	for (let i = 0; i < array.length;) {
 		let item = array[i];
 
-		item.classList.add('show');
+		item.classList.add('showSlide');
 		let promise = new Promise((resolve, reject) => {
 			setTimeout(() => resolve(item), 5000);
 		});
 		let result = await promise;
-		result.classList.remove('show');
+		result.classList.remove('showSlide');
 
 		(i === array.length - 1) ? i = 0 : i++;
 	}
 
 }
 
-intervalLog(imageArray);
+// const imageArray = gallery.querySelectorAll('.project__pic');
+
+for (const gallery of galleryArray) {
+	gallery.addEventListener('click', () => gallery.classList.toggle('showFull'));
+	const imageArray = gallery.querySelectorAll('.project__pic');
+	slideShow(imageArray);
+}
+// slideShow(imageArray);
