0c730bc731bc827846ab3e7bff9b199f7bc6aa38
SVA2021 <sofronov.vit@gmail.com>
2022-04-21 16:58:53 +0400

    auto slideshow added for img gallery

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@ headerMenuBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 	headerMenu.classList.toggle('menu__modal');
 });
+
+//*******************PROJECT **********************/
+
+const gallery = document.body.querySelector('.project__gallery');
+const imageArray = gallery.querySelectorAll('.project__pic');
+
+const intervalLog = async (array) => {
+
+	for (let i = 0; i < array.length;) {
+		let item = array[i];
+
+		item.classList.add('show');
+		let promise = new Promise((resolve, reject) => {
+			setTimeout(() => resolve(item), 5000);
+		});
+		let result = await promise;
+		result.classList.remove('show');
+
+		(i === array.length - 1) ? i = 0 : i++;
+	}
+
+}
+
+intervalLog(imageArray);
