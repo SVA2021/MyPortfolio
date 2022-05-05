d8111de6b4ac5c295d86c32ed061f50ee5cd3e7a
SVA2021 <sofronov.vit@gmail.com>
2022-05-05 12:39:24 +0400

    contacts form validaton added with js

--- js/script.js
+++ js/script.js
@@ -94,3 +94,49 @@ const shakeIconsOrder = (array) => {
 };
 
 // setInterval(() => shakeIconsOrder(icons) , 15000);
+
+//*******************FORM VALIDATION **********************/
+
+const contactForm = document.forms.contacts;
+// const nameInput = contactForm.elements.name;
+// const messageInput = contactForm.elements.message;
+const emailInput = contactForm.elements.email;
+const emailTip = contactForm.querySelector('.form__tooltip');
+// console.log(emailTip);
+// console.log(nameInput.validity.valid);
+// console.log(emailInput.value === " ");
+// console.log(messageInput.value.length);
+
+const checkEmail = () => {
+	let eMail = emailInput.value;
+	let index = eMail.indexOf('@');
+	let domen = eMail.slice(++index);
+	let isValid = domen.includes('.') && !domen.endsWith('.');
+	return isValid;
+}
+
+emailInput.addEventListener('blur', (e) => {
+	let isValid = checkEmail();
+	if (!isValid) {
+		emailTip.classList.add('form__error');
+		emailInput.classList.add('form__invalid');
+		emailInput.focus();
+		return false;
+	}
+	emailInput.classList.remove('form__invalid');
+	emailTip.classList.remove('form__error');
+	return false;
+})
+
+contactForm.addEventListener('submit', (e) => {
+	let eMail = emailInput.value;
+	let index = eMail.indexOf('@');
+	let domen = eMail.slice(++index);
+	let isValid = domen.includes('.');
+	if (!isValid) {
+		console.log('mail=', eMail, ' i=', index, ' d=', domen, 'valid=', isValid);
+		e.preventDefault();
+		emailInput.focus();
+		return false;
+	}
+})
