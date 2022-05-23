6d76ba9571395172c5a350432ea649bbd8fe27c3
SVA2021 <sofronov.vit@gmail.com>
2022-05-23 09:57:38 +0400

    upd: actual project content loading unify

--- js/script_project.js
+++ js/script_project.js
@@ -1,27 +1,30 @@
 'use strict'
 //*******************PROJECT ACTUAL**********************/
 
-/**
- * @param {set qty of pictures} qty 
- * @param {name of folder where files placed} name 
- * @param {ID of div where will be places all pictures} wrapperDivID 
- * @returns {fullfilled div with gallery or pictures of project}
- * @NB_1 pictures must be in placed as in template folder 
- * @NB_2 this script must be placed BEFORE main script 
- * @NB_3 "0" picture recomment be with performance test result
- * @NB_4 "1" picture recomment be main screen of project
- */
-const setGallery = (qty, name, wrapperDivID) => {
+const WEB_SKILLS_FULL = ['html5', 'css3', 'js', 'git', 'markdown', 'cssModules', 'github', 'react', 'redux', 'sass', 'typescript'];
 
-	if (!qty) return false;
-	let wrapper = document.getElementById(wrapperDivID);
+//! update this array below to add new project
+const PROJECT_ACTUAL = [
+	{ name: 'css_edu', imgQty: 9, webSkills: ['html5', 'git', 'cssModules', 'github', 'react', 'redux', 'sass', 'typescript',] },
+	{ name: 'raw', imgQty: 6, webSkills: ['html5', 'git', 'cssModules', 'github', 'react', 'redux', 'sass', 'typescript',] },
+	{ name: 'galeoncar', imgQty: 17, webSkills: ['html5', 'css3', 'js', 'git', 'markdown', 'github', 'sass',] },
+]
+
+const setProject = (project) => {
+	let name = project.name;
+	let qty = project.imgQty;
+	let webSkills = project.webSkills;
+
+	let wrapper = document.getElementById(name);
+	let gallery = wrapper.querySelector('.project__gallery');
+	let skills = wrapper.querySelector('.project__technologies');
 
 	for (let i = 0; i < qty; i++) {
 
 		let alt = (i === 0) ? `test results` :
 			(i === 1) ? `main screen` : `additional screenshot`;
 
-		wrapper.insertAdjacentHTML('beforeend',
+		gallery.insertAdjacentHTML('beforeend',
 			`<picture class="project__pic">
 			<source media="(max-width: 420px)" srcset="./img/${name}/360/${name}_${i}_360.webp" type="image/webp">
 			<source media="(max-width: 420px)" srcset="./img/${name}/360/${name}_${i}_360.png" type="image/png">
@@ -31,8 +34,14 @@ const setGallery = (qty, name, wrapperDivID) => {
 			<img class="project__img" alt="project ${alt}" loading="lazy"
 				src="./img/${name}/origin/${name}_${i}.png">`);
 	}
+
+	for (const item of webSkills) {
+		skills.insertAdjacentHTML('beforeend',
+			`<svg class="icon__svg" role="img" aria-label="js"><use href="#${item}"></use></svg>`
+		);
+	}
 }
 
-setGallery(9, 'css_edu', 'css_edu');
-setGallery(6, 'raw', 'raw');
-setGallery(17, 'galeoncar', 'galeoncar');
+for (const item of PROJECT_ACTUAL) {
+	setProject(item);
+}
