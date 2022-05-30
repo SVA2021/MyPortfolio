4bdbe39d33528bd8c217a6be595984c0b47c53a0
SVA2021 <sofronov.vit@gmail.com>
2022-05-30 20:19:45 +0400

    upd: delete unused scripts

--- js/script_initial.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	console.log('mobile');
- } else {
-	 console.log('desktop');
- }
